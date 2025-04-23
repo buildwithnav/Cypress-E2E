ba1e703d2aa5add86a1a78491942fa241b086bc1
nsgill-wabbit <nav.gill747@gmail.com>
2025-04-23 09:39:43 -0700

    Added mocha-awesome-report feature for test runs

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,15 @@
 const { defineConfig } = require("Cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports'
+  },
   e2e: {
     baseUrl:"https://naveenautomationlabs.com/opencart/index.php?route=account/login",// 🔥 Set your base URL here
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 
