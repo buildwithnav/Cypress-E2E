548331d00241287b6244c4c31c28fb18670030f9
nsgill-wabbit <nav.gill747@gmail.com>
2025-04-23 14:13:27 -0700

    minor fix

--- cypress/e2e/resigtrationFlow.cy.js
+++ cypress/e2e/resigtrationFlow.cy.js
@@ -1,4 +1,4 @@
-/// <reference types="Cypress" />
+/// <reference types="cypress" />
 import testDataRegisterPage from "../fixtures/testDataRegisterPage.json"
 import { registerPage } from "../pages/registerPage.cy.js";
 const registerObj = new registerPage();
